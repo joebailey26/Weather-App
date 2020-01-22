d4e473d936ad57984c69e6ea9b800756c30dba74
Joe <joe@joebailey.xyz>
2020-01-22 19:56:05 +0000

    Added ability to change background color based on weather

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,7 +38,39 @@ window.addEventListener('load', ()=> {
                     let node = document.createTextNode(format);
                     sup.appendChild(node);
                     temperatureDegree.appendChild(sup);
-                };                
+                };
+
+                if (celsius < 0) {
+                    document.body.style.setProperty('--color-one', '#4078C0');
+                }
+                else if (celsius < 10) {
+                    document.body.style.setProperty('--color-one', '#BAB9B9');
+                }
+                else if (celsius < 20) {
+                    document.body.style.setProperty('--color-one', '#FEEC37');
+                }
+                else if (celsius < 30) {
+                    document.body.style.setProperty('--color-one', '#F38B1E');
+                }
+                else if (celsius < 30) {
+                    document.body.style.setProperty('--color-one', '#F0050F');
+                }
+
+                if (icon === "clear-day") {
+                    document.body.style.setProperty('--color-two', '#2C38A7');
+                }
+                else if (icon === "clear-night") {
+                    document.body.style.setProperty('--color-two', '#1C293A');
+                }
+                else if (icon === "rain") {
+                    document.body.style.setProperty('--color-two', '#264184');
+                }
+                else if (icon === "snow", "sleet") {
+                    document.body.style.setProperty('--color-two', '#FFFFFF');
+                }
+                else {
+                    document.body.style.setProperty('--color-two', '#B4BAC6');
+                }
 
                 // Set Icon
                 setIcons(icon, document.querySelector('.icon'));
